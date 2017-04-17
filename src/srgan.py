Fix batch normalization layer

src/srgan.py
@@ -113,8 +113,8 @@ class SRGAN:
 
     def inference_losses(self, x, imitation, true_output, fake_output):
         def inference_content_loss(x, imitation):
-            _, x_phi = vgg.build_model(x, None, True)
-            _, imitation_phi = vgg.build_model(imitation, None, True)
+            _, x_phi = vgg.build_model(x, tf.constant(False), True)
+            _, imitation_phi = vgg.build_model(imitation, tf.constant(False), True)
             content_loss = None
             for i in range(len(x_phi)):
                 if content_loss is None:

utils/layer.py
@@ -40,15 +40,16 @@ def batch_normalize(x, is_training, decay=0.99, epsilon=0.001, trainable=True):
     scale = tf.get_variable('scale', dtype=tf.float32, shape=[dim], initializer=tf.truncated_normal_initializer(stddev=0.1), trainable=trainable)
     pop_mean = tf.get_variable('pop_mean', dtype=tf.float32, shape=[dim], initializer=tf.constant_initializer(0.0), trainable=False)
     pop_var = tf.get_variable('pop_var', dtype=tf.float32, shape=[dim], initializer=tf.constant_initializer(1.0), trainable=False)
-    if is_training is not None:
-        mean, var = tf.nn.moments(x, axes=[0, 1, 2])
+    def bn_train():
         batch_mean, batch_var = tf.nn.moments(x, axes=[0, 1, 2])
-        tf.assign(pop_mean, pop_mean * decay + batch_mean * (1 - decay))
-        tf.assign(pop_var, pop_var * decay + batch_var * (1 - decay))
-        return tf.nn.batch_normalization(x, batch_mean, batch_var, beta, scale, epsilon)
-    else:
+        train_mean = tf.assign(pop_mean, pop_mean * decay + batch_mean * (1 - decay))
+        train_var = tf.assign(pop_var, pop_var * decay + batch_var * (1 - decay))
+        with tf.control_dependencies([train_mean, train_var]):
+            return tf.nn.batch_normalization(x, batch_mean, batch_var, beta, scale, epsilon)
+    def bn_inference():
         return tf.nn.batch_normalization(x, pop_mean, pop_var, beta, scale, epsilon)
-                                                                                                            
+    return tf.cond(is_training, bn_train, bn_inference)
+
 def flatten_layer(x):
     input_shape = x.get_shape().as_list()
     dim = input_shape[1] * input_shape[2] * input_shape[3]

vgg19/train.py
@@ -22,7 +22,7 @@ def train():
     train_op = opt.minimize(model.loss, global_step=global_step)
     init = tf.global_variables_initializer()
     sess.run(init)
-    
+
     if tf.train.get_checkpoint_state('backup/'):
         saver = tf.train.Saver()
         saver.restore(sess, 'backup/latest')
@@ -30,7 +30,7 @@ def train():
     x_train, t_train = load('./cifar_100/train')
     x_test, t_test = load('./cifar_100/test')
 
-    n_iter = np.int64(np.ceil(len(x_train) / batch_size))
+    n_iter = int(np.ceil(len(x_train) / batch_size))
     while True:
         epoch = int(sess.run(global_step) / n_iter)
         print('----- epoch {} -----'.format(epoch+1))
@@ -52,19 +52,19 @@ def train():
         print('accuracy: {} %'.format(test_accuracy * 100))
 
 
-
 def validate(x, t, model, sess):
     prediction = np.array([])
     answer = np.array([])
     for i in range(0, len(x), batch_size):
         x_batch = IMG.augment(x[i:i+batch_size])
         t_batch = t[i:i+batch_size]
-        output = model.out.eval(feed_dict={model.x: x_batch}, session=sess)
+        output = model.out.eval(feed_dict={model.x: x_batch, model.is_training: False}, session=sess)
         prediction = np.concatenate([prediction, np.argmax(output, 1)])
         answer = np.concatenate([answer, t_batch])
         correct_prediction = np.equal(prediction, answer)
     accuracy = np.mean(correct_prediction)
     return accuracy
+
 
 if __name__ == '__main__':
     train()

vgg19/vgg19.py
@@ -11,7 +11,7 @@ class VGG19:
         self.trainable = trainable
         self.x = tf.placeholder(tf.float32, [None, self.img_dim, self.img_dim, 3])
         self.t = tf.placeholder(tf.int32, [None])     
-        self.is_training = tf.placeholder(tf.bool)
+        self.is_training = tf.placeholder(tf.bool, [])
         self.out, self.phi = self.build_model(self.x, self.is_training)
         self.loss = self.inference_loss(self.out, self.t)
 