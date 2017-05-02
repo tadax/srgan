b2aa2d9a0aeeaca49b352612f07ad8b9c674e55a
tadax <tadax@xtn.jp>
2017-05-02 14:55:58 +0900

    Modify cifar

--- vgg19/cifar_100/download.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# CIFAR-100
-wget -O 'cifar-100-python.tar.gz' 'https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz'
-tar -xvzf cifar-100-python.tar.gz
-mv cifar-100-python raw
-rm cifar-100-python.tar.gz
-
