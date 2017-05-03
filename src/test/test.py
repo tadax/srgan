acfe6e75689bf705dd3687d54f950e56297e4ff6
tadax <tadax@xtn.jp>
2017-05-04 04:34:32 +0900

    Modify test

--- src/test/test.py
+++ src/test/test.py
@@ -1,5 +1,7 @@
 import numpy as np
+import scipy.misc
 import cv2
+import dlib
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import sys
@@ -8,12 +10,10 @@ sys.path.append('../../utils')
 sys.path.append('../../vgg19')
 from srgan import SRGAN
 
-batch_size = 16
-
 x = tf.placeholder(tf.float32, [None, 96, 96, 3])
 is_training = tf.placeholder(tf.bool, [])
 
-model = SRGAN(x, is_training, batch_size)
+model = SRGAN(x, is_training, 16)
 sess = tf.Session()
 init = tf.global_variables_initializer()
 sess.run(init)
@@ -21,10 +21,22 @@ sess.run(init)
 saver = tf.train.Saver()
 saver.restore(sess, '../backup/latest')
 
-img = cv2.imread('test.jpg')
-img = img / 127.5 - 1
-input_ = np.zeros((batch_size, 96, 96, 3))
-input_[0] = img
+img = cv2.imread('temp.jpg')
+h, w = img.shape[:2]
+detector = dlib.get_frontal_face_detector()
+dets = detector(img, 1)
+if dets is None or len(dets) != 1:
+    print("unsuitable")
+    exit()
+d = dets[0]
+if d.left() < 0 or d.top() < 0 or d.right() > w or d.bottom() > h:
+    print("unsuitable")
+    exit()
+face = img[d.top():d.bottom(), d.left():d.right()]
+face = cv2.resize(face, (96, 96))
+face = face / 127.5 - 1
+input_ = np.zeros((16, 96, 96, 3))
+input_[0] = face
 
 mos, fake = sess.run(
     [model.downscaled, model.imitation],
@@ -43,4 +55,9 @@ for j, img in enumerate([mos[0], fake[0], input_[0]]):
     plt.gca().get_yaxis().set_ticks_position('none')
     plt.xlabel(labels[j])
 plt.savefig('result.jpg')
+
+c = cv2.imread('result.jpg')
+c = c[160:340, 70:585, :]
+c = cv2.cvtColor(c, cv2.COLOR_BGR2RGB)
+scipy.misc.imsave('result.jpg', c)
 
