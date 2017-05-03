74c7edab6f10be15735240e9aaea82f7c6a94088
tadax <tadax@xtn.jp>
2017-05-04 05:13:07 +0900

    Modify layer.py

--- utils/layer.py
+++ utils/layer.py
@@ -132,17 +132,11 @@ def pixel_shuffle_layer(x, r, n_split):
         x = tf.reshape(x, (bs, a, b, r, r))
         x = tf.transpose(x, (0, 1, 2, 4, 3))
         x = tf.split(x, a, 1)
-        #x = tf.split(1, a, x)
         x = tf.concat([tf.squeeze(x_) for x_ in x], 2)
-        #x = tf.concat(2, [tf.squeeze(x_) for x_ in x])
         x = tf.split(x, b, 1)
-        #x = tf.split(1, b, x)
         x = tf.concat([tf.squeeze(x_) for x_ in x], 2)
-        #x = tf.concat(2, [tf.squeeze(x_) for x_ in x])
         return tf.reshape(x, (bs, a*r, b*r, 1))
 
     xc = tf.split(x, n_split, 3)
-    #xc = tf.split(3, n_split, x)
     return tf.concat([PS(x_, r) for x_ in xc], 3)
-    #return tf.concat(3, [PS(x_, r) for x_ in xc])
     
